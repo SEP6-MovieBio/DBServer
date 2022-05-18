97a05b85744301bf066471abc685511487216b9b
kasper <kasperfriangt@gmail.com>
2022-05-18 12:53:23 +0200

    error handling for exception

--- DBServer/DBAccess/DBContext.cs
+++ DBServer/DBAccess/DBContext.cs
@@ -11,44 +11,55 @@ namespace DBServer.DBAccess
     {
         public List<MovieInfo> GetMovieInfo()
         {
-            string connectionString;
-            SqlConnection connection;
-            NetworkCredential creds = new NetworkCredential(Environment.GetEnvironmentVariable("DbUsername"),Environment.GetEnvironmentVariable("DbPassword"));
+            List<MovieInfo> list = new List<MovieInfo>();
+            try
+            {
+                string connectionString;
+                SqlConnection connection;
+                NetworkCredential creds = new NetworkCredential(Environment.GetEnvironmentVariable("DbUsername"),Environment.GetEnvironmentVariable("DbPassword"));
 
-            SecureString secureString = creds.SecurePassword;
-            secureString.MakeReadOnly();
+                SecureString secureString = creds.SecurePassword;
+                secureString.MakeReadOnly();
 
-            connectionString = "Data Source=dbsep6.database.windows.net;initial Catalog=moviedb";
+                connectionString = "Data Source=dbsep6.database.windows.net;initial Catalog=moviedb";
                 /*$"jdbc:sqlserver://dbsep6.database.windows.net:1433;database=moviedb;user=ad@dbsep6;password={password};" +
                 "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";*/
             
-            connection = new SqlConnection(connectionString, new SqlCredential(creds.UserName, secureString));
+                connection = new SqlConnection(connectionString, new SqlCredential(creds.UserName, secureString));
             
-            List<MovieInfo> list = new List<MovieInfo>();
+                
             
-            SqlCommand command;
-            SqlDataReader reader;
-            string sql = "";
-            string output = "";
+                SqlCommand command;
+                SqlDataReader reader;
+                string sql = "";
+                string output = "";
 
-            connection.Open();
+                connection.Open();
 
-            sql = "Select top(10) title, [year], director, rating, votes from moviedb.dbo.movieinfo";
+                sql = "Select top(10) title, [year], director, rating, votes from moviedb.dbo.movieinfo";
             
-            command = new SqlCommand(sql, connection);
+                command = new SqlCommand(sql, connection);
             
-            reader = command.ExecuteReader();
+                reader = command.ExecuteReader();
 
-            while (reader.Read())
-            {
-                MovieInfo movieInfo = new MovieInfo(reader.IsDBNull(0)?"Unknown":reader.GetString(0), reader.IsDBNull(1)? 0: reader.GetDecimal(1), reader.IsDBNull(2)?"Unknown":reader.GetString(2),
-                    reader.IsDBNull(3)?0:reader.GetSqlSingle(3).Value, reader.IsDBNull(4)?0:reader.GetInt32(4));
-                list.Add(movieInfo);
+                while (reader.Read())
+                {
+                    MovieInfo movieInfo = new MovieInfo(reader.IsDBNull(0)?"Unknown":reader.GetString(0), reader.IsDBNull(1)? 0: reader.GetDecimal(1), reader.IsDBNull(2)?"Unknown":reader.GetString(2),
+                        reader.IsDBNull(3)?0:reader.GetSqlSingle(3).Value, reader.IsDBNull(4)?0:reader.GetInt32(4));
+                    list.Add(movieInfo);
+                }
+
+                connection.Close();
+                
+                return list;
             }
+            catch (Exception e)
+            {
+                list.Add(new MovieInfo(e.Message.ToString(),0,"unknown",0,0));
+                return list;
 
-            connection.Close();
-
-            return list;
+            }
+           
         }
     }
 }

91227909721a48368c7a6d86e57d56bd8f3f2921
Crisiluluman <kawazu-maner@hotmail.com>
2022-05-24 12:46:32 +0200

    Added search for specific movie

--- DBServer/DBAccess/DBContext.cs
+++ DBServer/DBAccess/DBContext.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Security;
+using System.Threading.Tasks;
+using DBServer.Models;
 
 
 namespace DBServer.DBAccess
@@ -41,8 +44,12 @@ namespace DBServer.DBAccess
 
                 while (reader.Read())
                 {
-                    MovieInfo movieInfo = new MovieInfo(reader.IsDBNull(0)?"Unknown":reader.GetString(0), reader.IsDBNull(1)? 0: reader.GetDecimal(1), reader.IsDBNull(2)?"Unknown":reader.GetString(2),
-                        reader.IsDBNull(3)?0:reader.GetSqlSingle(3).Value, reader.IsDBNull(4)?0:reader.GetInt32(4));
+                    MovieInfo movieInfo = new MovieInfo(
+                        reader.IsDBNull(0)?"Unknown":reader.GetString(0), 
+                        reader.IsDBNull(1)? 0: reader.GetDecimal(1), 
+                        reader.IsDBNull(2)?"Unknown":reader.GetString(2),
+                        reader.IsDBNull(3)?0:reader.GetSqlSingle(3).Value, 
+                        reader.IsDBNull(4)?0:reader.GetInt32(4));
                     list.Add(movieInfo);
                 }
 
@@ -58,5 +65,101 @@ namespace DBServer.DBAccess
             }
            
         }
+
+        public async Task<Movie> GetMovieById(int id)
+        {
+            Movie movie = new Movie();
+            List<MovieReview> reviews = new List<MovieReview>();
+            List<string> directors = new List<string>();
+            
+            //string sql = $"Select id, m.title, m.[year], director, rating, votes, s.star from dbo.movies m inner join dbo.movieInfo mi on mi.title like m.title and  mi.[year] = m.[year] left join dbo.starInfo s on s.movieAppearedIn like m.title where id = {id}";
+
+            string sql = $"  select [MovieID], [title], [year], [Director], [rating], [votes] from [dbo].[movieInfo] where [MovieID] = {id}";
+            try
+            {
+                NetworkCredential creds = new NetworkCredential(Environment.GetEnvironmentVariable("DbUsername"),Environment.GetEnvironmentVariable("DbPassword"));
+
+                string connectionString;
+                SqlConnection connection;
+                SecureString secureString = creds.SecurePassword;
+                secureString.MakeReadOnly();
+                    
+                connectionString = Environment.GetEnvironmentVariable("connString");
+     
+                connection = new SqlConnection(connectionString, new SqlCredential(creds.UserName, secureString));
+
+                SqlCommand command;
+                SqlDataReader reader;
+
+                connection.Open();
+
+                command = new SqlCommand(sql, connection);
+            
+                reader = command.ExecuteReader();
+                
+                while(reader.Read())
+                {
+                    directors.Add(reader.IsDBNull(3)?"Unknown":reader.GetString(3));
+                    movie = new Movie(
+                        reader.IsDBNull(0)?0:reader.GetInt32(0),
+                        reader.IsDBNull(1)?"Unknown":reader.GetString(1),
+                        reader.IsDBNull(2)? 0: reader.GetDecimal(2),
+                        directors,
+                        reader.IsDBNull(4)?0:reader.GetSqlSingle(4).Value,
+                        reader.IsDBNull(5)?0:reader.GetInt32(5),
+                        0,
+                        reviews
+                    );
+                
+                }
+
+                connection.Close();
+                
+                return movie;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e);
+
+                return new Movie();
+            }
+
+        }
+
+        
+        
+        //Testing with object between servers
+        /*
+        public async Task<Movie> GetDummyMovie()
+        {
+            MovieReview review1 = new MovieReview("Crisseren", "Too good to be true", 8.9);
+            MovieReview review2 = new MovieReview("Toothbrook", "It was OK", 10);
+            MovieReview review3 = new MovieReview("Ling", "Meh", 4);
+
+            List<MovieReview> reviews = new List<MovieReview>();
+            reviews.Add(review1);
+            reviews.Add(review2);
+            reviews.Add(review3);
+            
+            List<string> genreTag = new List<string>();
+            genreTag.Add("Horror");
+            genreTag.Add("Action");
+            genreTag.Add("Romance");
+            
+            List<string> directors = new List<string>();
+            directors.Add("James");
+            directors.Add("TheBond");
+            directors.Add("Shaken");
+            
+            List<string> starred = new List<string>();
+            starred.Add("Toothbrook");
+            starred.Add("Ling");
+            starred.Add("Crisseren");
+
+            Movie dummyMovie = new Movie(1,"TestMovie",2009,"imageString","This movie good",genreTag, 12,7,4,directors,starred, reviews);
+            return dummyMovie;
+        }
+        */
     }
 }
---
+++ DBServer/Models/MovieReview.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DBServer.Models
+{
+    public class MovieReview
+    {
+        public MovieReview(string reviewUsername, string reviewDescription, double reviewRating)
+        {
+            this.ReviewUsername = reviewUsername;
+            this.ReviewDescription = reviewDescription;
+            this.ReviewRating = reviewRating;
+        }
+        
+        [JsonPropertyName("reviewUsername")]
+        public string ReviewUsername { get; set; }
+
+        [JsonPropertyName("reviewDescription")]
+        public string ReviewDescription { get; set; }
+
+        [JsonPropertyName("reviewRating")]
+        public double ReviewRating { get; set; }
+
+        
+        public string ToString()
+        {
+            // Converts the values into jsonFormat
+
+
+            return "{"
+                   + "\"reviewUsername\":" + "\"" + ReviewUsername + "\","
+                   + "\"reviewDescription\":" + "\"" + ReviewDescription + "\","
+                   + "\"reviewRating\":" + "\"" + ReviewRating + "\","
+                   + "}";
+        }
+    }
+}
